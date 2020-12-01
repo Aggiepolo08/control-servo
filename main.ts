3d6b7101468a1167fc3f91ccf5d969d6520eb1df
Aggiepolo08 <73924964+Aggiepolo08@users.noreply.github.com>
2020-11-30 19:30:32 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
-    pins.servoWritePin(AnalogPin.P8, 102)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
-    pins.servoWritePin(AnalogPin.P8, 0)
 })
+pins.servoWritePin(AnalogPin.P4, 30)
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        pins.servoWritePin(AnalogPin.P4, 30)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P4, 49)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P4, 68)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P4, 87)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P4, 110)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P4, 130)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P4, 145)
+        basic.pause(5000)
+    }
 })
